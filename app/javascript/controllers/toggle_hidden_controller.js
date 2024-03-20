382fdc8829266ebc82bd858c60a0a83249accf9e
Zachary Williams <zackwilldev@gmail.com>
2024-03-20 11:47:15 -0400

    new stimulus controller to hide/show portions of the form

---
+++ app/javascript/controllers/toggle_hidden_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle-hidden"
+export default class extends Controller {
+  static targets = [ "id", "password" ]
+
+  connect(){
+
+  }
+
+  showPasswordFields(event) {
+    event.preventDefault
+
+    this.passwordTargets.forEach(passwordTarget => {
+      passwordTarget.classList.remove("hidden");
+    });
+
+    this.idTargets.forEach(idTarget => {
+      idTarget.classList.add("hidden");
+    });
+  }
+
+  showIdFields(event) {
+    console.log("HERE")
+    event.preventDefault
+
+    this.passwordTargets.forEach(passwordTarget => {
+      passwordTarget.classList.add("hidden");
+    });
+
+    this.idTargets.forEach(idTarget => {
+      idTarget.classList.remove("hidden");
+    });
+  }
+}
