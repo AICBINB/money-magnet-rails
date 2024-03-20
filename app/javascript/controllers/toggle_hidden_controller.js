7379ea4ef67a83a750f2761390afda1009807cc8
Zachary Williams <zackwilldev@gmail.com>
2024-03-20 12:13:27 -0400

    unneeded function

--- app/javascript/controllers/toggle_hidden_controller.js
+++ app/javascript/controllers/toggle_hidden_controller.js
@@ -4,10 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "id", "password" ]
 
-  connect(){
-
-  }
-
   showPasswordFields(event) {
     event.preventDefault
 
