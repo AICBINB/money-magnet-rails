983bc113b892daec7806ad63acb04566635bd9ad
Donte Ladatto <donte279@gmail.com>
2024-03-23 22:11:01 -0500

    seed template

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,50 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Clearing database..."
+
+Budget.destroy_all
+User.destroy_all
+Goal.destroy_all
+Expense.destroy_all
+
+puts "Seeding budgets..."
+
+b1 = Budget.create(amount: 2500.00)
+
+puts "Seeding users..."
+
+kara = User.create(email: 'moneyluvr@moneymagnet.com', encrypted_password: 'demo123', first_name: 'Kara', last_name: "Flynn", role: "", access_token: "", budget_id: b1.id)
+
+puts "Seeding expenses..."
+
+kara.expenses.create([
+  {
+    description: "",
+    current: 0.0,
+    total: 0.0,
+    recurring: true,
+    due: DateTime.new(2024, 4, 21)
+  },
+  {
+    description: "",
+    current: 0.0,
+    total: 0.0,
+    recurring: true,
+    due: DateTime.new(2024, 4, 21)
+  }
+])
+
+puts "Seeding goals..."
+
+kara.expenses.create([
+  {
+    description: "",
+    current: 0.0,
+    total: 0.0
+  },
+  {
+    description: "",
+    current: 0.0,
+    total: 0.0
+  }
+])
+
+puts "Done!"
