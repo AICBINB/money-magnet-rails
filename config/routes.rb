40d15df896b3e6c5bcb87b571dad43b374b305c8
Donte Ladatto <donte279@gmail.com>
2024-03-24 17:01:15 -0500

    advice route

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
     }
   
   get 'plaid/new'
+
+  # Ask ChatGPT for financial advice. The request body should look like this:
+  # {
+  #   text: "A paragraph containing information about the budget and the income, expenses, and goals of the user(s)"
+  # }
+  post "/advice", to: "advice#advise"
+  
 end
