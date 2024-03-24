7acc8c9a83fda6b9a596a172a5166597277db2dc
Donte Ladatto <donte279@gmail.com>
2024-03-24 11:48:59 -0500

    add admin method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_many :expenses
+  has_many :goals
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  def admin?
+    role == "admin"
+  end
 end
