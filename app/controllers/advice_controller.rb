6fe9bbd8a016bed05eade0ddaa7776ba61c14279
Donte Ladatto <donte279@gmail.com>
2024-03-25 15:30:35 -0500

    upodate for ruby views

--- app/controllers/advice_controller.rb
+++ app/controllers/advice_controller.rb
@@ -1,9 +1,21 @@
 class AdviceController < ApplicationController
   wrap_parameters format: []
+  skip_before_action :verify_authenticity_token
 
-  def advise
-    advice = AdviceService.new(advice_params).advise
-    render json: advice.to_json, status: :ok
+  def new
+  end
+
+  def create
+    # @advice = AdviceService.new(advice_params).advise
+    respond_to do |format| 
+      format.turbo_stream do 
+        render turbo_frame: turbo_stream.update(
+          "Hiya",
+          partial: "/advice/show",
+          locals: "hello"
+        )
+      end
+    end
   end
 
   private
--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,24 +1,21 @@
 class BudgetsController < ApplicationController
 
   def index
-    render json: Budget.all, status: :ok
+    @budgets = Budget.all, status: :ok
   end
 
   def create
-    budget = Budget.create!(budget_params)
-    render json: budget, status: :created
+    @budget = Budget.create!(budget_params)
   end
 
   def show
-    budget = Budget.find_by(id: params[:id])
-    render json: budget, status: :ok
+    @budget = Budget.find_by(id: params[:id])
   end
 
   def update
     if current_user.admin?
       budget = current_user.budget
       budget.update!(budget_params)
-      render json: budget, status: :ok
     else render json: {error: "You do not have permission to make changes to the budget!"}
     end
   end
