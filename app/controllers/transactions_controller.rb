ce42ed7cbdcfe28a95ef5a2ee3e1b5d66bb38469
Donte Ladatto <donte279@gmail.com>
2024-03-25 21:28:04 -0500

    updated index action

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,10 @@
 class TransactionsController < ApplicationController
+
   def index
-    @transactions = Transaction.all
+    client = Plaid::PlaidApi.new
+    request = Plaid::ItemPublicTokenExchangeRequest.new
+    transactions_sync_request = Plaid::TransactionsSyncRequest.new
+    transactions_sync_request.access_token = access_token
+    @transactions = transaction_response.added
   end
 end
